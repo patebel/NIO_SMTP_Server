d97fa9d3446156da60f50486730de621f3ec7627
ASUS <ASUS@ASUS-PC.localdomain>
2017-05-05 17:04:17 +0200

    Bums Code mit Selector eingefügt

--- src/application/smtpserver.java
+++ src/application/smtpserver.java
@@ -1,41 +1,81 @@
 package application;
 
-import java.io.FileOutputStream;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class smtpserver {
 
 	public static void main(String[] argv) throws Exception {
 
-		// TODO Auto-generated method stub
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 
-		// rename with correct data
-		String servername = null;
-		int port = 80;
+		serverSocketChannel.socket().bind(new InetSocketAddress(9999));
 
-		// Declaration; Socket, input, output
-		ServerSocketChannel sc = ServerSocketChannel.open();
+		Selector selector = Selector.open();
 
-		sc.socket().bind(new InetSocketAddress(servername, port));
-		sc.configureBlocking(false);
-
-		// size?
-		int capacity = 8192;
-		String filepath = "./user/test.txt";
-		ByteBuffer bbuf = ByteBuffer.allocate(capacity);
-		FileOutputStream fos = new FileOutputStream(filepath);
+		serverSocketChannel.configureBlocking(false);
 
 		while (true) {
-			SocketChannel socketChannel = sc.accept();
+			SocketChannel socketChannel = serverSocketChannel.accept();
 
 			if (socketChannel != null) {
-				// hier die Buffer verwenden?
-			}
-		}
 
+				SelectionKey key = socketChannel.register(selector, SelectionKey.OP_CONNECT);
+
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+
+				Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+				while (keyIterator.hasNext()) {
+
+					SelectionKey ourkey = keyIterator.next();
+
+					if (ourkey.isAcceptable()) {
+						// a connection was accepted by a ServerSocketChannel.
+
+					} else if (ourkey.isConnectable()) {
+						// a connection was established with a remote server.
+
+					} else if (ourkey.isReadable()) {
+						// a channel is ready for reading
+
+					} else if (ourkey.isWritable()) {
+						// a channel is ready for writing
+					}
+
+					keyIterator.remove();
+				}
+
+			}
+
+			/*
+			 * // TODO Auto-generated method stub
+			 * 
+			 * // rename with correct data String servername = null; int port =
+			 * 80;
+			 * 
+			 * // Declaration; Socket, input, output ServerSocketChannel sc =
+			 * ServerSocketChannel.open();
+			 * 
+			 * sc.socket().bind(new InetSocketAddress(servername, port));
+			 * sc.configureBlocking(false);
+			 * 
+			 * // size? int capacity = 8192; String filepath =
+			 * "./user/test.txt"; ByteBuffer bbuf =
+			 * ByteBuffer.allocate(capacity); FileOutputStream fos = new
+			 * FileOutputStream(filepath);
+			 * 
+			 * while (true) { SocketChannel socketChannel = sc.accept();
+			 * 
+			 * if (socketChannel != null) { // hier die Buffer verwenden? } }
+			 */
+		}
 	}
 
 }
